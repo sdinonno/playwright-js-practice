8666d8ef3aa98be68d47483ae7ab54ed4a81be4c
Sofia Di Nonno <sofia@thelucky.app>
2023-05-06 15:53:51 -0300

    Using mouse events with Playwright

---
+++ basic_scripts/mouse.js
@@ -0,0 +1,17 @@
+const { chromium } = require('playwright');
+
+( async() => {
+    const browser = await chromium.launch({headless:false, slowMo:400});
+    const page = await browser.newPage();
+    await page.goto('https://paint.js.org');
+
+    await page.mouse.move(200, 200);
+    await page.mouse.down();
+    await page.mouse.move(400, 200);
+    await page.mouse.move(400, 400);
+    await page.mouse.move(200, 400);
+    await page.mouse.move(200, 200);
+    await page.mouse.up();
+
+    await browser.close();
+})();
